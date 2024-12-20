5aa85655186f39a4db5db4baf7c509704c449390
JoanP-B <peraltajoan36@gmail.com>
2024-12-20 18:05:04 -0500

    estructura proyecto principal

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,9 +16,39 @@ $(window).on("scroll", function() {
 //Documentacion typed
 
 $(document).ready(function() {
+
+    //typed animacion
     new Typed('#type-it', {
         strings: ['Personal','Empresarial','Educativo','Ejecutivo'],
         typeSpeed: 200,
         loop: true
+    });
+
+    // owl animacion
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 2,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            900: {
+                items: 2
+            },
+        }
+    });
+
+    //One Page Scroll
+    $.scrollIt({
+        upKey: 38,
+        downKey: 40,
+        easing: 'linear',
+        scrollTime: 100,
+        activeClass: 'active',
+        onPageChange: null,
+        topOffset: 0
     });
 });
